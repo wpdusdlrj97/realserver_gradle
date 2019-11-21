16d2c627b32ee5c7b73097d397f3617a064b88ac
wpdusdlrj97 <48294315+wpdusdlrj97@users.noreply.github.com>
2019-11-21 22:06:23 +0900

    Password Encoder Bcrypt X -> Sha로 변경

---
+++ src/main/java/com/example/realserver_gradle/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.example.realserver_gradle.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class MemberController {
+
+
+    // 로그아웃 결과 페이지
+    @GetMapping("/logout/result")
+    public String dispLogout() {
+        return "/logout";
+    }
+
+
+}
