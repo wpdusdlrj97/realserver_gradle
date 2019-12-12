8822086b256c42ba2ad6e594fecda0cc572b605d
wpdusdlrj97 <48294315+wpdusdlrj97@users.noreply.github.com>
2019-12-12 19:33:23 +0900

    로그인페이지

--- src/main/java/com/example/realserver_gradle/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/realserver_gradle/service/UserDetailsServiceImpl.java
@@ -55,64 +55,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		System.out.println(user.getPassword());
 
 
-		/*
-		try {
-			sendPost(user.getUsername());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		 */
-
-
-
-
 		return user;
 	}
 
 
-
-	/*
-
-	private void sendPost(String getemail) throws Exception {
-		URL url = new URL("http://49.247.136.36/fitme_session.php"); // 호출할 url
-		Map<String,Object> params = new LinkedHashMap<>(); // 파라미터 세팅
-		params.put("email", getemail);
-
-		StringBuilder postData = new StringBuilder();
-		for(Map.Entry<String,Object> param : params.entrySet()) {
-			if(postData.length() != 0) postData.append('&');
-			postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-			postData.append('=');
-			postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-		}
-		byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-
-		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-		conn.setRequestMethod("POST");
-		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-		conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-		conn.setDoOutput(true);
-		conn.getOutputStream().write(postDataBytes); // POST 호출
-
-		int responseCode = conn.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-
-		String inputLine;
-		while((inputLine = in.readLine()) != null) { // response 출력
-			System.out.println(inputLine);
-		}
-
-		in.close();
-	}
-	*/
-
-
-
-
+	
 
 
 
